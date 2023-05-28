62ae4d3018e495666dbc7cf6ce7eee25dbb07536
Kacper <kacpersjuszb@gmail.com>
2023-05-28 10:53:39 +0200

    BotAuth and SignatureChecker

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+
+    public static String fileName = "blockchain.data";
+
+    public static void main(String[] args) {
+
+        Blockchain blockchain = BlockchainManager.retrieveOrCreateBlockchain(fileName);
+        blockchain.setFileName(fileName);
+
+        BlockchainManager blockchainManager = new BlockchainManager(blockchain);
+
+        int nbOfBlocksToAdd = 5;
+        int nbOfThreads = 6;
+
+        System.out.printf("Adding %d blocks...%n", nbOfBlocksToAdd);
+        blockchainManager.startBlockchain(nbOfThreads, nbOfBlocksToAdd);
+        System.out.println("Is blockchain valid? " + BlockchainValidator.isBlockchainValid());
+
+        System.out.println();
+        System.out.println(blockchain.toString());
+    }
+}
